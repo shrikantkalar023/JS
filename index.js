7b67543a3b7afa3ee4d7d6ca44a1249000768495
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-02-20 20:16:20 +0530

    learned about Vars

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-//This is my 1st JS code
-console.log("Hello World");
+let firstName = "shri";
+console.log(firstName);
+
+//cant be a reserved keyword
+//should be meaningful
+//cant start with a no (1name)
+//cant contain a space or hyphen (-)
+//Case-sensitive
+//use camelCase
+
+let lastName = "Kalar";
