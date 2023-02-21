d60ae728a25b6325719758a5c31023cda9bf9056
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-02-21 18:53:02 +0530

    learnt assignment operators

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 let x = 10;
-let y = 3;
 
-// console.log(x + y);
-// console.log(x - y);
-// console.log(x * y);
-// console.log(x / y);
-// console.log(x % y);
-// console.log(x ** y);
-
-//Increment (++)
-console.log(x++);
-console.log(x);
-
-//Decrement (--)
-console.log(--x);
+x = x + 5;
+x += 5;
