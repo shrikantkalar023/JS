f951dc1bc91a1456b5eed3bc29fca31fd8fedbd0
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-02-21 18:43:33 +0530

    learned basics of functions part2

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// performing a task
 function greet(name, lastName) {
   console.log("Hello " + name + " " + lastName);
 }
 
-greet("John", "Doe");
+// Calculating a value
+function square(number) {
+  return number * number;
+}
+
+console.log(2 + 2);
 
 // argument is value supplied.
 //parameter is var in fn declration
