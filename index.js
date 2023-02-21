b7c09466d335791d88bd20017c652ef510a9f53e
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-02-21 18:55:48 +0530

    learnt Comparison operators

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-let x = 10;
+let x = 1;
 
-x = x + 5;
-x += 5;
+// Relational
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 0);
+console.log(x <= 1);
+
+// Equality
+console.log(x !== 1);
