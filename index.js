b8489e918a5ebb17eb23a53ac7757bcbb0ee4599
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-02-23 05:13:06 +0530

    If Else statement

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-// 1=00000001 1 byte ig
-// 2=00000010
-// 3=00000011
+// // Hour
+// If betn 6am & 12pm : Gm
+// If betn 12pm & 5pm: Ga
+// else: GE
 
-console.log(1 | 2); // Bitwise OR
-console.log(1 & 2); // Bitwise AND
+let hour = 10;
 
-// Read, Write, Execute
-// 00000100
-// 00000010
-// 00000001
-
-const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
-
-let myPermission = 0;
-myPermission = myPermission | readPermission | writePermission;
-console.log(myPermission & writePermission ? "YES" : "NO");
+if (hour >= 6 && hour < 12) {
+  console.log("Good morning!");
+} else if (hour > 12 && hour < 18) {
+  console.log("Good afternoon!");
+} else {
+  console.log("Good evening!");
+}
