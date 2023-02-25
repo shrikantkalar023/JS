ad48b6ba8f982255d2768d7889bb9b6a8e3f76cd
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-02-25 15:20:49 +0530

    Continue & Break keywords

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-// For loop
-
-for (let i = 5; i >= 1; i--) {
-  if (i % 2 !== 0) console.log(i);
-}
-
-// While Loop
 let i = 0;
-while (i <= 5) {
-  if (i % 2 !== 0) console.log(i);
+while (i <= 10) {
+  // if (i === 5) break;
+  if (i % 2 === 0) {
+    i++;
+    continue;
+  }
+  console.log(i);
   i++;
-}
-
-// Do while...condition is evaluated later
-let j = 9;
-do {
-  if (j % 2 !== 0) console.log(j);
-  j++;
-} while (j <= 5);
-
-// for-in loop
-const person = {
-  name: "mosh",
-  age: 30,
-};
-
-for (const key in person) {
-  console.log(key, ":", person[key]);
-}
-
-// for-of loop
-const colors = ["red", "green", "blue"];
-
-for (const color of colors) {
-  console.log(color);
 }
