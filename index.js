cd7efefbbd3748fa3f862dd9da859cb6c0f94293
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-02-26 12:18:21 +0530

    fizzBuzz program

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function maxof2no(no1, no2) {
-  // if (no1 > no2) return no1;
-  // return no2;
-  return no1 > no2 ? no1 : no2;
+function fizzBuzz(input) {
+  if (typeof input !== "number") return NaN;
+  if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
+  if (input % 3 === 0) return "Fizz";
+  if (input % 5 === 0) return "Buzz";
+  return input;
 }
 
-console.log(maxof2no(4, 1));
-console.log(maxof2no(2, 3));
-console.log(maxof2no(2, 2));
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(15));
+console.log(fizzBuzz(7));
+console.log(fizzBuzz(false));
