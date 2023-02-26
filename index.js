eaa1d97e3fbd2789f1a2398b831df0a3f35e4925
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-02-27 00:58:32 +0530

    even & odd nos

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-function checkSpeed(speed) {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
-  if (speed < speedLimit + kmPerPoint) return "Ok";
-  const points = Math.floor((speed - 70) / kmPerPoint);
-  if (speed > speedLimit) return points < 12 ? points : "License Suspended";
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i++) {
+    const message = i % 2 === 0 ? "Even" : "Odd";
+    console.log(i, message);
+  }
 }
 
-console.log(checkSpeed(69));
-console.log(checkSpeed(70));
-console.log(checkSpeed(71));
-console.log(checkSpeed(75));
-console.log(checkSpeed(77));
-console.log(checkSpeed(83));
-console.log(checkSpeed(90));
-console.log(checkSpeed(130));
+showNumbers(10);
