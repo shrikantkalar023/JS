c0c3118b19cd80ab1d5809c3af3a7b17c981a881
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-01 02:45:23 +0530

    dynamic nature of objects

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-// Factory Fn uses camelCase
-function createCricle(radius) {
-  return {
-    radius,
-    draw() {
-      console.log("draws");
-    },
-  };
-}
+const circle = {
+  radius: 1,
+};
 
-c1 = createCricle(1);
-console.log(c1);
+circle.color = "red";
+circle.draw = function () {};
 
-// Constructor Fn uses PascalCase
-function Circle(radius) {
-  this.radius = radius;
-  this.draw = function () {
-    console.log("drawc");
-  };
-}
+delete circle.color;
+delete circle.draw;
 
-const c2 = new Circle(2);
-console.log(c2);
-
-// new operator
-// 1. creates new empty obj like (const x={})
-// 2. this points to our new empty obj.
-// 3. returns new obj from the Constructor fn
+console.log(circle);
