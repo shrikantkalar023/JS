c957635c08cda83ab7212410ef26fa587a79fc93
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-01 02:12:33 +0530

    basics of Object

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-showPrimes(20);
+// Object-oriented Programming (OOP)
 
-function showPrimes(limit) {
-  for (let num = 2; num <= limit; num++) {
-    if (isPrime(num)) console.log(num);
-  }
-}
+const circle = {
+  radius: 1, //can be any type of value in (key:value pair)
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: true,
+  draw: function () {
+    console.log("draw");
+  },
+};
 
-function isPrime(num) {
-  for (let factor = 2; factor <= num / 2; factor++)
-    if (num % factor === 0) return false;
-  return true;
-}
+circle.draw(); // Method
