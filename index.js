43f914a0a8d8341ad9d2944227a0783e6c4e1fa1
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-03 16:17:03 +0530

    Diff betn primitives and ref types

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-// in JS Fns are Objs.
-const square = {
-  side: 4,
-};
+let x = { value: 10 };
+let y = x;
 
-function Circle(radius) {
-  this.radius = radius;
-  this.draw = function () {
-    console.log("draw");
-  };
+x.value = 20;
+
+// Primitives are copied by their value
+// Objects are copied by their reference (address)
+
+function increase(noobj) {
+  noobj.value++;
 }
 
-const another = new Circle(1);
-
-// Internally this happens when new keyword is used
-c1 = Circle.call({}, 1);
-c2 = Circle.apply({}, [1, 2, 3]);
-
-// Internally this happens when we declare a Fn
-// const Circle1 = new Function(
-//   "radius",
-//   `this.radius = radius;
-//   this.draw = function () {
-//     console.log("draw");
-//   };`
-// );
-
-// const c1 = new Circle1(1);
+let no = { value: 10 };
+increase(no);
+console.log(no);
