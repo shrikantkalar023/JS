debd5fea9bb74217051547fbec04b5c33a32479f
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-06 03:37:27 +0530

    exercise 2 blog Post obj

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-// Constructor Fn
-function AddressC(street, city, zipCode) {
-  this.street = street;
-  this.city = city;
-  this.zipCode = zipCode;
-}
-const address1 = new AddressC(2, "Nagpur", 440038);
-const address2 = new AddressC(2, "Nagpur", 440038);
-let address3 = address2;
+blogPost1 = {
+  title: "How to xyz",
+  body: "smthing",
+  author: "rahul",
+  views: 1234321,
+  comments: [
+    { author: "name1", body: "sm text" },
+    { author: "name2", body: "sm text 2" },
+  ],
+  isLive: true,
+};
 
-// address2.district = "nagpur";
-
-function areSame(obj1, obj2) {
-  return obj1 === obj2;
+function BlogPost(title, body, author) {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.views = 0;
+  this.comments = [];
+  this.isLive = false;
 }
 
-function areEqual(obj1, obj2) {
-  let l1 = Object.keys(obj1);
-  let l2 = Object.keys(obj2);
-  for (let i = 0; i < Math.max(l1.length, l2.length); i++) {
-    if (obj1[l1[i]] !== obj2[l2[i]]) return false;
-  }
-  return true;
-}
-console.log(areEqual(address1, address2));
-console.log(areSame(address1, address2));
-console.log(areSame(address3, address2));
+const blogPost2 = new BlogPost("sm title", "sm text", "name1");
+
+console.log(blogPost1);
+console.log(blogPost2);
