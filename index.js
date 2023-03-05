9105b9b84ff985e14a4d6c2f34d861c798ca0f2d
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-06 01:06:46 +0530

    Exercise Obj declaration

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-const now = new Date();
-const date1 = new Date("March 3 2022 09:00");
-const date2 = new Date(2022, 2, 2, 9);
+// Constructor Fn
+function AddressC(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
+}
+const Address1 = new AddressC(2, "Nagpur", 440038);
+const Address2 = {
+  street: 1,
+  city: "Los Angeles",
+  zipCode: 440028,
+};
 
-now.getDate();
-date1.setFullYear(2021);
+// Factory Function
+function addressF(street, city, zipCode) {
+  return {
+    street,
+    city,
+    zipCode,
+  };
+}
+
+const Address3 = addressF(3, "New York", 239818);
+
+function Showaddress(obj) {
+  for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+      console.log(key, obj[key]);
+    }
+  }
+}
+Showaddress(Address1);
+Showaddress(Address2);
+Showaddress(Address3);
