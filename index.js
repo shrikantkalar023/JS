89966081e61e6cb36ec0f6bc0a739a4e5dd38b31
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-07 21:43:37 +0530

    spread operator for combining and slicing

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let first = [{ id: 1 }];
+let first = [1, 2, 3];
 let second = [4, 5, 6];
 
-first[0].id = 10;
-
-combiend = first.concat(second);
+// combiend = first.concat(second);
+const combiend = [...first, "a", ...second, "b"];
 console.log(combiend);
 
-// const slice = combiend.slice(2, 4);
-const slice = combiend.slice(2);
-console.log(slice);
+// const copy = combiend.slice();
+const copy = [...combiend];
+
+console.log(copy);
