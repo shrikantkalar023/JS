615e59ed4fb9601c8fd359084457a418b6da1b25
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-08 18:20:49 +0530

    Filtering an array (filter method)

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 // some checks for at least one element
 const num = [1, 2, -1, 3];
 
-const allPositive = num.every((val) => val > 0);
-const atLeastOnePositive = num.some((val) => val > 0);
+const filtered = num.filter((val) => val >= 0);
 
-console.log(allPositive);
-console.log(atLeastOnePositive);
+console.log(filtered);
