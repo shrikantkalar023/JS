03b6e1c3c48deb97de0b55e2f668fc135d52ef33
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-08 18:27:59 +0530

    Mapping an array (map method) primitive type

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// every & some method
-// every checks if every element matches condition
-// some checks for at least one element
 const num = [1, 2, -1, 3];
 
 const filtered = num.filter((val) => val >= 0);
 
-console.log(filtered);
+const items = filtered.map((val) => "<li>" + val + "</li>");
+
+const html = "<ul>" + items.join("") + "<ul>";
+console.log(items);
+console.log(html);
