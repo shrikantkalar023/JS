ce601a0a6368881c082658d5f6e82b60dbbd9da2
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-08 18:17:45 +0530

    testing elements of array (every and some method)

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-// sorting array primitive type
-// let numbers = [2, 1, 3];
+// every & some method
+// every checks if every element matches condition
+// some checks for at least one element
+const num = [1, 2, -1, 3];
 
-// numbers.sort();
-// console.log(numbers);
+const allPositive = num.every((val) => val > 0);
+const atLeastOnePositive = num.some((val) => val > 0);
 
-// numbers.reverse();
-// console.log(numbers);
-
-// sorting array ref type
-let courses = [
-  { id: 1, name: "Node" },
-  { id: 2, name: "javaScript" },
-];
-
-courses.sort(function (a, b) {
-  // a>b=>1
-  // a<b=>-1
-  // a===b=>0
-  const nameA = a.name.toUpperCase();
-  const nameB = b.name.toUpperCase();
-
-  if (nameA > nameB) return 1;
-  if (nameA < nameB) return -1;
-  return 0;
-});
-console.log(courses);
+console.log(allPositive);
+console.log(atLeastOnePositive);
