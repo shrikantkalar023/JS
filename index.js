ad2ffc769a5aba7976d3c03f4927d0e262478c10
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-11 03:03:59 +0530

    Exercise 4 move ele shri

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const nums = [0, 1, 2, 3, 4, 5, 1, 1, 2, 2, 9];
+const nums = [0, 1, 2, 3, 4, 5];
 
-function except(array, excluded) {
-  let output = [];
-  for (const ele of array) {
-    if (!excluded.includes(ele)) output.push(ele);
+function move(array, index, offset) {
+  const output = [];
+  const newIndex = index + offset;
+
+  if (offset >= 0) {
+    if (newIndex > array.length - 1) return errorMsg();
+
+    for (let i = 0; i < array.length; i++) {
+      if ((i >= 0 && i < index) || (i > newIndex && i < array.length))
+        output[i] = array[i];
+      else if (i >= index && i < newIndex) output[i] = array[i + 1];
+      else output[newIndex] = array[index];
+    }
+  } else {
+    if (newIndex < 0) return errorMsg();
+
+    for (let i = 0; i < array.length; i++) {
+      if ((i >= 0 && i < newIndex) || (i > index && i < array.length))
+        output[i] = array[i];
+      else if (i > newIndex && i <= index) output[i] = array[i - 1];
+      else output[newIndex] = array[index];
+    }
   }
-  return output;
+  return console.log(output);
 }
 
-console.log(except(nums, [1, 2]));
+function errorMsg() {
+  return console.error("Invalid offset");
+}
+
+move(nums, 0, 1);
