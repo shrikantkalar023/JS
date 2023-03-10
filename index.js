fa23a17ddccd4c4396ab0291e894aabd7c25f9f5
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-11 03:13:38 +0530

    Exercise 4 move ele smarter way

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
 const nums = [0, 1, 2, 3, 4, 5];
 
 function move(array, index, offset) {
-  const output = [];
-  const newIndex = index + offset;
-
-  if (offset >= 0) {
-    if (newIndex > array.length - 1) return errorMsg();
-
-    for (let i = 0; i < array.length; i++) {
-      if ((i >= 0 && i < index) || (i > newIndex && i < array.length))
-        output[i] = array[i];
-      else if (i >= index && i < newIndex) output[i] = array[i + 1];
-      else output[newIndex] = array[index];
-    }
-  } else {
-    if (newIndex < 0) return errorMsg();
-
-    for (let i = 0; i < array.length; i++) {
-      if ((i >= 0 && i < newIndex) || (i > index && i < array.length))
-        output[i] = array[i];
-      else if (i > newIndex && i <= index) output[i] = array[i - 1];
-      else output[newIndex] = array[index];
-    }
+  const position = index + offset;
+  if (position < 0 || position >= array.length) {
+    console.error("Invalid offset");
+    return;
   }
-  return console.log(output);
+
+  const output = [...array];
+  const ele = output.splice(index, 1)[0];
+  output.splice(position, 0, ele);
+  return output;
 }
 
-function errorMsg() {
-  return console.error("Invalid offset");
-}
-
-move(nums, 0, 1);
+console.log(move(nums, 0, -1));
