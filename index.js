a124eec996a534f9275fa6028bbb8f71649caf20
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-12 02:58:28 +0530

    exercise 5

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const nums = [0, 1, 2, 3, 4, 5];
+const nums = [0, 1, 4, 2, 3, 4, 5, 4];
 
-function move(array, index, offset) {
-  const position = index + offset;
-  if (position < 0 || position >= array.length) {
-    console.error("Invalid offset");
-    return;
-  }
+const count = countOccurrences(nums, 4);
 
-  const output = [...array];
-  const ele = output.splice(index, 1)[0];
-  output.splice(position, 0, ele);
-  return output;
+function countOccurrences(array, searchEle) {
+  //   let count = 0;
+  //   for (let i of array) {
+  //     if (i === searchEle) count++;
+  //   }
+  // return count;
+
+  return array.reduce((accumulator, current) => {
+    // const occurrence = current === searchEle ? 1 : 0;
+    if (current === searchEle) accumulator++;
+    console.log(accumulator, current, searchEle);
+    return accumulator;
+    // return accumulator + occurrence;
+  }, 0);
 }
 
-console.log(move(nums, 0, -1));
+console.log(count);
