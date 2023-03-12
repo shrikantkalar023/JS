4f76108a8ce714d7e5d3040cc8beac3e74ea1b5d
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-13 01:49:08 +0530

    hoisting in JS

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Fn declaration
+walk();
+
 function walk() {
   console.log("walk");
 }
 
 // Anonymous Fn expression
+run();
+
 let run = function () {
   console.log("run");
 };
 
-run();
-let move = run;
-move();
+// 'Hoisting' in JS is moving up all the fn declarations to the top of the file.
+// it is done by automaticallly JS engine executing the code.
