e88da7cd7518d33e122da3987fda03771056ecf5
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-13 01:44:15 +0530

    two ways to declare Fns

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-const movies = [
-  { title: "a", year: 2018, rating: 4.5 },
-  { title: "b", year: 2018, rating: 4.7 },
-  { title: "c", year: 2018, rating: 3 },
-  { title: "d", year: 2017, rating: 4.5 },
-];
+// Fn declaration
+function walk() {
+  console.log("walk");
+}
 
-let result = movies
-  .filter((value) => value.year === 2018 && value.rating > 4)
-  .sort((a, b) => a.rating - b.rating)
-  .reverse()
-  .map((m) => m.title);
+// Anonymous Fn expression
+let run = function () {
+  console.log("run");
+};
 
-// result.sort(function (a, b) {
-//   if (a.rating > b.rating) return 1;
-//   if (a.rating < b.rating) return -1;
-//   return 0;
-// });
-
-// result.reverse();
-
-// result = result.map((value) => value.title);
-
-console.log(result);
+run();
+let move = run;
+move();
