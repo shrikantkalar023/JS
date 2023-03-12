e00359bf7a960d17506f2ed2f67ace49fa46153d
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-13 02:06:29 +0530

    fn having unlimited arguments.

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-// Fn declaration
-walk();
-
-function walk() {
-  console.log("walk");
+function sum() {
+  let total = 0;
+  for (const val of arguments) {
+    total += val;
+  }
+  return total;
 }
 
-// Anonymous Fn expression
-run();
-
-let run = function () {
-  console.log("run");
-};
-
-// 'Hoisting' in JS is moving up all the fn declarations to the top of the file.
-// it is done by automaticallly JS engine executing the code.
+console.log(sum(20, 4, 8));
