821db2d94dfb1f1bc20f9a3acf63535704b7a494
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-13 18:49:40 +0530

    basics of error handling

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
+// try and catch. Error handling.
 const person = {
   firstName: "Shrikant",
   lastName: "Kalar",
-  get fullname() {
+  get fullName() {
     return `${person.firstName} ${person.lastName}`;
   },
-  set fullname(value) {
+
+  set fullName(value) {
+    if (typeof value !== "string") {
+      throw new Error("Value is not a string");
+    }
     const parts = value.split(" ");
+    if (parts.length != 2) {
+      throw new Error("Enter a first and last name.");
+    }
+    // lines after throw are not run if error is caught.
     this.firstName = parts[0];
     this.lastName = parts[1];
   },
 };
 
-person.fullname = "Ashwini Kalar";
+try {
+  person.fullName = "";
+} catch (e) {
+  // console.log(e);
+  alert(e); //alert is old way. Use red label here instead.
+}
+
 console.log(person);
 
-// getters=> access properties
-// setters=> change (mutate) them
+// throwing an error makes it exception.
