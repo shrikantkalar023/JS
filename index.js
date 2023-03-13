0a43c7351b50eefbd77a41ef5da615630d4b203d
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-14 04:02:15 +0530

    this keyword p2

--- index.js
+++ index.js
@@ -4,22 +4,12 @@
 
 const video = {
   title: "a",
-  play: function () {
-    console.log(this);
+  tags: ["a", "b", "c"],
+  showTags: function () {
+    this.tags.forEach(function (tag) {
+      console.log(this.title, tag); //this is referencing global window obj here
+    }, this); //this refs video obj bsc we in showTags()
   },
 };
 
-video.stop = function () {
-  console.log(this);
-};
-
-// video.play();
-// video.stop();
-// console.log(video);
-
-function Video1(title) {
-  this.title = title;
-  console.log(this); // references new empty obj
-}
-
-const v = new Video1("sd");
+video.showTags();
