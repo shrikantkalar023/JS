c11b079138d9d43b40a9cd721830275f9f3b8d9f
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-14 04:19:56 +0530

    changing value of 'this' p2 with arrow fns

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 const video = {
   title: "a",
   tags: ["a", "b", "c"],
-  showTags: function () {
-    // const self = this; //not preferred but works
-    this.tags.forEach(
-      function (tag) {
-        console.log(this.title, tag);
-      }.bind(this)
-    );
+  showTags() {
+    this.tags.forEach((tag) => console.log(this.title, tag));
   },
 };
 
 video.showTags();
 
-function playVideo(a, b) {
-  console.log(this);
-}
-
-playVideo.call({ name: "shrikant" }, 1, 2);
-playVideo.apply({ name: "shrikant" }, [1, 2]);
-// const fn = playVideo.bind({ name: "shrikant" }, [1, 2]);
-// fn();
-playVideo.bind({ name: "shrikant" }, [1, 2])();
-playVideo();
+// arrow fns inherit this value from containing fn
