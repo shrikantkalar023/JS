3bd9af52fef4d50b1041e3b4e0b4d64414d4b413
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-15 03:16:54 +0530

    Exercise 2 read only obj property

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-function sum(...num) {
-  if (num.length === 1 && Array.isArray(num[0])) num = [...num[0]];
-  return num.reduce((a, b) => a + b);
-}
+let circle = {
+  radius: 3,
+  get area() {
+    return Math.PI * this.radius * this.radius;
+  },
+};
 
-console.log(sum([1, 2, 3, 4]));
+console.log(circle);
