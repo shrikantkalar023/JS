45da49f4644d4fd64bc875dce4c0ba63884c7064
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-15 03:43:07 +0530

    exercise 3 error handling

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-let circle = {
-  radius: 3,
-  get area() {
-    return Math.PI * this.radius * this.radius;
-  },
-};
+const nums = [1, 3, 3, 2, 4];
 
-console.log(circle);
+try {
+  const count = countOccurrences(true, 3);
+  console.log(count);
+} catch (e) {
+  console.log(e.message);
+}
+
+function countOccurrences(array, searchEle) {
+  if (!Array.isArray(array))
+    throw new Error("Please enter array of numbers only");
+  return array.reduce((accumulator, current) => {
+    if (current === searchEle) accumulator++;
+    return accumulator;
+  }, 0);
+}
