63af351039fa80429a4124d556f1f06a427c2230
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-17 02:05:24 +0530

    another way of implementing getters and setters

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-// private methods & properties ...implementing Abstraction
-
 function Circle(radius) {
   this.radius = radius;
-  let defLocation = { x: 0, y: 0 };
 
-  let computeOptimalLocation = function (factor) {
-    // ....
+  let defaultLocation = { x: 0, y: 0 };
+
+  this.getDefaultLocation = function () {
+    return defaultLocation;
   };
-  this.draw = function () {
-    computeOptimalLocation(1);
-    // defLocation
-    // this.radius
 
+  this.draw = function () {
     console.log("drawC");
   };
+
+  Object.defineProperty(this, "defaultLocationGetter", {
+    get: function () {
+      return defaultLocation;
+    },
+
+    set: function (value) {
+      if (!value.x || !value.y) throw new Error("Invalid location");
+      defaultLocation = value;
+    },
+  });
 }
 
 let circle = new Circle(10);
