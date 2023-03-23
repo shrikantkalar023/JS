757d4d3e758cd512f43eb581d63ec7fbd0712cc1
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-23 19:34:00 +0530

    Implemented Prototypical Inheritance (class & obj)

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-function Circle(radius) {
-  // Instance members
-  this.radius = radius;
+function Shape() {}
 
-  this.move = function () {
-    console.log("move");
-  };
+Shape.prototype.duplicate = function () {
+  console.log("duplicate");
+};
+
+function Circle(radius) {
+  this.radius = radius;
 }
 
-// Prototype members
+Circle.prototype = Object.create(Shape.prototype);
+
 Circle.prototype.draw = function () {
   console.log("draw");
 };
 
-const c1 = new Circle(1);
-
-// Only returns insance members
-console.log(Object.keys(c1));
-
-// returns all members (instance  + prototype)
-for (const key in c1) {
-  console.log(key);
-}
+let c = new Circle(1);
+let s = new Shape();
