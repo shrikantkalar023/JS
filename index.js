97a7ed0fad3835b7f35f1f06427e6b90fdeb4b82
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-24 04:30:05 +0530

    Intermediate Function Inheritance

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+function extend(Child, Parent) {
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
+}
+
 function Shape(color) {
   this.color = color;
 }
@@ -11,12 +16,21 @@ function Circle(radius, color) {
   this.radius = radius;
 }
 
-Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;
+extend(Circle, Shape);
 
 Circle.prototype.draw = function () {
   console.log("draw");
 };
+
+function Square(size, color) {
+  // Shape.call(this, color);
+  this.size = size;
+}
+
+extend(Square, Shape);
+
+Square.prototype = Object.create(Shape.prototype);
+Square.prototype.constructor = Square;
 
 let s = new Shape();
 let c = new Circle(1, "red");
