e618c8a8d1eb1ee5c946e6e364d92e6114ad2f14
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-25 03:01:44 +0530

    Mehod overriding

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 function extend(Child, Parent) {
+  // Child.prototype= Object.create(Object.prototype)
   Child.prototype = Object.create(Parent.prototype);
   Child.prototype.constructor = Child;
 }
 
-function Shape(color) {
-  this.color = color;
-}
+function Shape() {}
 
 Shape.prototype.duplicate = function () {
   console.log("duplicate");
 };
 
-function Circle(radius, color) {
-  Shape.call(this, color);
+function Circle(radius) {
   this.radius = radius;
 }
 
 extend(Circle, Shape);
 
-Circle.prototype.draw = function () {
-  console.log("draw");
+Circle.prototype.duplicate = function () {
+  Shape.prototype.duplicate.call(this);
+  console.log("duplicate circle");
 };
 
-function Square(size, color) {
-  // Shape.call(this, color);
-  this.size = size;
-}
-
-extend(Square, Shape);
-
-Square.prototype = Object.create(Shape.prototype);
-Square.prototype.constructor = Square;
-
-let s = new Shape();
-let c = new Circle(1, "red");
+let c = new Circle(1);
