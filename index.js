985d4b837de3f0e6ac402a15475504ba730f90e9
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-26 02:59:41 +0530

    ES6 classes

--- index.js
+++ index.js
@@ -1,51 +1,25 @@
-function HtmlElement() {
-  this.click = function () {
-    console.log("clicked");
-  };
+// OG way
+// function Circle(radius) {
+//   this.radius = radius;
+
+//   this.draw = function () {
+//     console.log("draw");
+//   };
+// }
+
+// new ES6 classes
+// these r essentially Constructor fns
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+    this.move = function () {
+      console.log("move");
+    };
+  }
+
+  draw() {
+    console.log("draw");
+  }
 }
 
-HtmlElement.prototype.foucs = function () {
-  console.log("focused");
-};
-
-function HtmlSelectElement(items = []) {
-  this.items = items;
-
-  this.addItem = function (item) {
-    this.items.push(item);
-  };
-
-  this.removeItem = function (item) {
-    if (this.items.includes(item))
-      this.items.splice(this.items.indexOf(item), 1);
-  };
-
-  this.render = function () {
-    return `<select>${this.items
-      .map((item) => `<option>${item}</option>`)
-      .join("")}</select>`;
-  };
-}
-
-HtmlSelectElement.prototype = new HtmlElement();
-HtmlSelectElement.prototype.constructor = HtmlSelectElement;
-
-function HtmlImageElement(src) {
-  this.src = src;
-
-  this.render = function () {
-    return `<img src="${this.src}" />`;
-  };
-}
-
-HtmlImageElement.prototype = new HtmlElement();
-HtmlImageElement.prototype.constructor = HtmlImageElement;
-
-const elements = [
-  new HtmlSelectElement([1, 2, 3]),
-  new HtmlImageElement("http://"),
-];
-
-for (const i of elements) {
-  console.log(i.render());
-}
+c = new Circle(1);
