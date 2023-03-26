71c323cf7a14b95c573fdb2e6847b1e0bea3b37b
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-03-27 03:17:26 +0530

    ES6 Cleaner getter & setter syntax

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-// using WeakMaps for Private properties & methods
-
 const _radius = new WeakMap();
-// WeakMap is a dict with obj as keys.
-const _move = new WeakMap();
 
 class Circle {
   constructor(radius) {
     _radius.set(this, radius);
-
-    _move.set(this, () => console.log("move", this));
   }
 
-  draw() {
-    // console.log(_radius.get(this));
-    _move.get(this)();
-    console.log("draw");
+  // new ES6 cleaner syntax
+  get radius() {
+    return _radius.get(this);
+  }
+
+  set radius(value) {
+    if (value <= 0) throw new Error("invalid radius");
+    _radius.set(this, value);
   }
 }
-
 const c = new Circle(1);
 console.log(c);
-c.draw();
